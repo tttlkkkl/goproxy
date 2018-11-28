fe0d0d5e12cc99692fed826890fab43b4318d6e9
li hua <hs@insucrm.com>
2018-11-28 17:49:49 +0800

    尝试

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 		os.MkdirAll(modTmpDir, 0755)
 		createFile(modTmpDir)
 	}
-	os.Chdir(modTmpDir)
+	//os.Chdir(modTmpDir)
 	if _, err := os.Stat(cacheDir); os.IsNotExist(err) {
 		fmt.Fprintf(os.Stdout, "goproxy: %s cache dir is not exist. %s\n", time.Now().Format("2006-01-02 15:04:05"), cacheDir)
 		os.MkdirAll(cacheDir, 0755)
@@ -103,8 +103,8 @@ func mainHandler(inner http.Handler) http.Handler {
 }
 
 func goGet(path, version, suffix string, w http.ResponseWriter, r *http.Request) error {
-	//dir := modInit()
-	//os.Chdir(dir)
+	dir := modInit()
+	os.Chdir(dir)
 	//defer func(dir string) {
 	//	modClear(dir)
 	//}(dir)
@@ -160,7 +160,6 @@ func goGet(path, version, suffix string, w http.ResponseWriter, r *http.Request)
 }
 func modInit() string {
 	tmpDir := path.Dir(modTmpDir) + "/" + UUID.String()
-	//os.Chdir(tmpDir)
 	err := os.MkdirAll(tmpDir, 0755)
 	if err != nil {
 		fmt.Println("临时目录创建失败:", err)
